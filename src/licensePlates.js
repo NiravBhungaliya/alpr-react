67a13d0f7f6c6201dde5106a44c33a8092878a8c
NiravBhungaliya <nirav.bhungaliya@ontario.ca>
2024-04-19 16:37:41 -0400

    Tab added in license plates

--- src/licensePlates.js
+++ src/licensePlates.js
@@ -1,6 +1,7 @@
 import { OntarioStepIndicator } from "@ontario-digital-service/ontario-design-system-component-library-react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import Tab from "./tab";
 
 const LicensePlate = () => {
   const location = useLocation();
@@ -12,14 +13,18 @@ const LicensePlate = () => {
     }
   }, [location, navigate]);
 
-  return (
+  return <>
     <OntarioStepIndicator
       show-back-button="true"
       back-button-url="../"
       currentStep="1"
       number-of-steps="1"
     ></OntarioStepIndicator>
-  );
+    <h1>Displaying the Result</h1>
+    <p>Following page differenciate accepted and not accepted License plates.</p>
+    <hr className="thick-border"></hr>
+    <Tab responseData={location.state.responseData}/>
+    </>
 };
 
 export default LicensePlate;
